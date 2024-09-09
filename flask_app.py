cc65081e66d79cd521e2ea54f374a40c23c2376d
studio boran <boranloves@naver.com>
2024-09-09 16:38:42 +0900

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,74 @@
 from flask import Flask, request, jsonify
-import shii
+import json
+from korcen import korcen
 
-shii.no_study_return_text("시이는 아무것도 몰라여..")
-shii.study_return_text("ㅇㅎ")
-                          
+no_study_return = "시이는 아무것도 몰라여.."
+study_return = "이해 했어요!"
 
 app = Flask(__name__)
+
+
+def load_bot_info():
+    try:
+        with open('bot_info.json', 'r', encoding='utf-8') as f:
+            bot_info = json.load(f)
+    except FileNotFoundError:
+        bot_info = {}
+
+    return bot_info
+
+
+def save_bot_info(bot_info):
+    with open('bot_info.json', 'w', encoding='utf-8') as f:
+        json.dump(bot_info, f, ensure_ascii=False, indent=4)
+
+
+def study(keyword: str, description: str):
+    '''Teaches the bot to respond to a keyword.'''
+    ''':return ex): study_return'''
+    bot_info = load_bot_info()
+    if 'https://' in description or 'https://' in keyword:
+        return "링크를 포함시키지 말아주세요..."
+    if 'http://' in description or 'http://' in keyword:
+        return "링크를 포함시키지 말아주세요..."
+    if 'discord.gg' in description or 'discord.gg' in keyword:
+        return "디스코드 서버 초대 링크를 포함시키지 말아주세요..."
+    if korcen.check(keyword) or korcen.check(description) or korcen.check(keyword + description) or korcen.check(description + keyword):
+        return "그런건... 배우기 싫어여.."
+    if keyword not in bot_info:
+        bot_info[keyword] = {
+            'description': description,
+        }
+        save_bot_info(bot_info)
+        if study_return == None:
+          return f"{keyword}를 학습하였습니다."
+        else:
+          return study_return
+    else:
+        return f"{keyword}는 이미 알고 있어요!"
+
+
+def study_say(keyword: str):
+    '''Returns the description of the keyword.'''
+    ''':return ex): 안녕하세요'''
+    bot_info = load_bot_info()
+    info = bot_info.get(keyword)
+
+    word = {
+          'shii': f"pip install -U shii",
+    }
+
+    if keyword in word.keys():
+        return f'{word[keyword]}'
+    else:
+        if info:
+            description = info['description']
+            return f"{description}"
+        else:
+          if no_study_return == None:
+            return f"{keyword}는 아직 모르겠어요.."
+          else:
+            return no_study_return
 
 
 @app.route("/")
@@ -19,7 +82,7 @@ def say():
 
     keywords = req["action"]["detailParams"]["keyword"]["value"]
     print(keywords)
-    a = shii.study_say(keywords)
+    a = study_say(keywords)
     b = a
 
     res = {
@@ -39,14 +102,13 @@ def say():
 
 
 @app.route("/study", methods=['POST'])
-def study():
+def studys():
     req = request.get_json()
 
     keyword = req["action"]["detailParams"]["keyword"]["value"]
     des = req["action"]["detailParams"]["des"]["value"]
-    user = req["action"]["detailParams"]["user"]["value"]
 
-    data = shii.study(keyword, des, user)
+    data = study(keyword, des)
 
     text = data
 
